46690cdef5632bf4ef6354a51e5980edcadad79e
Harry1o1 <ajju40959@gmail.com>
2021-12-05 16:58:36 +0600

    bugging

--- django/herokudjango/settings.py
+++ django/herokudjango/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'herokudjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
