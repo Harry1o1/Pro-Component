d656bce961bdfbf51b65fec5b5c0c13f9e6de66a
Harry1o1 <ajju40959@gmail.com>
2021-12-05 19:47:13 +0600

    booboo

--- django/herokudjango/settings.py
+++ django/herokudjango/settings.py
@@ -131,8 +131,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 django_heroku.settings(locals())
 STATIC_URL = '/static/'
