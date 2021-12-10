b23a6cea2486fb149171b94302984a461db0a743
Harry1o1 <ajju40959@gmail.com>
2021-12-10 22:16:19 +0600

    Nextjs pages111

--- myapp/pages/express.tsx
+++ myapp/pages/express.tsx
@@ -1,18 +1,14 @@
 
 
+
 function Express({data} : {data : any},{error}:{error : any}) {
   return (
-    <>
+   <>
         {data.map( (curElem:any) => {
             return(
                 <div className ="alert alert-primary" key={curElem.id}>
                     <h3>{curElem.name}</h3>
                     <h4>{curElem.email}</h4>
-                    <h5>{curElem.work}</h5>
-                    <h5>{curElem.phone}</h5>
-                    <h5>{curElem.password}</h5>
-                    <h5>{curElem.cpassword}</h5>
-                    <h6>{curElem.id}</h6>
                 </div>            
             );
         })}
@@ -58,3 +54,8 @@ export async function getStaticProps() {
 
 export default Express;
 
+                    // <h5>{curElem.Comment}</h5>
+                    // <h5>{curElem.bff}</h5>
+                    // <h5>{curElem.stars}</h5>
+                    // <h5>{curElem.age}</h5>
+                    // <h6>{curElem.id}</h6>
