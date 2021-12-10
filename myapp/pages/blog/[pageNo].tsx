83bcef1e42dc6beff2454f614de2237d67267907
Harry1o1 <ajju40959@gmail.com>
2021-12-10 19:33:28 +0600

    Nextjs pages1o1

---
+++ myapp/pages/blog/[pageNo].tsx
@@ -0,0 +1,19 @@
+import { useRouter } from 'next/router'
+
+
+
+
+
+
+
+export default function pageNo() {
+    const router = useRouter();
+    pageNumber = router.query.pageNo;
+    return (
+        <>
+            <h1>Hello from { pageNumber } page</h1>
+        </>
+    )
+};
+
+
