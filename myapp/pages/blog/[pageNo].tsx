baa537e06a76ae3b3bae423a2f8809cb659d7506
Harry1o1 <ajju40959@gmail.com>
2021-12-10 19:39:03 +0600

    Nextjs pages1o3

--- myapp/pages/blog/[pageNo].tsx
+++ myapp/pages/blog/[pageNo].tsx
@@ -8,7 +8,7 @@ import { useRouter } from 'next/router'
 
 export default function pageNo() {
     const router = useRouter();
-    pageNumber:any = router.query.pageNo;
+    const pageNumber = router.query.pageNo;
     return (
         <>
             <h1>Hello from { pageNumber } page</h1>
