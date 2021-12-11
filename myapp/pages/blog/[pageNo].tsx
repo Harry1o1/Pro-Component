2686d23207e18136fcb76ade2dbd7024218073f5
Harry1o1 <ajju40959@gmail.com>
2021-12-11 08:57:54 +0600

    Nextjs pages126

--- myapp/pages/blog/[pageNo].tsx
+++ myapp/pages/blog/[pageNo].tsx
@@ -17,7 +17,7 @@ export async function getStaticPaths() {
     }
 
 // This also gets called at build time
-export async function getStaticProps({ params:any }) {
+export async function getStaticProps({ params }) {
   // params contains the post `id`.
   // If the route is like /posts/1, then params.id is 1
       const res = await fetch(`https://.../posts/${params.id}`)
