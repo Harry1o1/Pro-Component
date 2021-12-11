4da3fe1dc400915eae0f2064f8c380bc5c210c2c
Harry1o1 <ajju40959@gmail.com>
2021-12-11 10:47:21 +0600

    Nextjs pages136

--- myapp/pages/blog/[pageNo].tsx
+++ myapp/pages/blog/[pageNo].tsx
@@ -1,43 +1,14 @@
 import { useRouter } from 'next/router'
-// import type { NextPage } from 'next'
-
-
-// This function gets called at build time
-export async function getStaticPaths() {
-          // Call an external API endpoint to get posts
-          const res = await fetch('https://pro-component-express1o1.herokuapp.com/register')
-          const posts = await res.json()
-          // Get the paths we want to pre-render based on posts
-          console.log(posts);
-          const paths = posts.map((post:any) => ({
-            params: { id: posts._id.toString()},
-          }))
-          // We'll pre-render only these paths at build time.
-          // { fallback: false } means other routes should 404.
-        return { paths, fallback: false };
-    }
-
-// This also gets called at build time
-export async function getStaticProps( params:any ) {
-  // params contains the post `id`.
-  // If the route is like /posts/1, then params.id is 1
-      const res = await fetch(`https://.../posts/${params.id}`)
-      const post = await res.json()
-    
-      // Pass post data to the page via props
-      return { props: { post } }
-}
 
 
 
-// const PageNo: NextPage = ({post}: {post:any}) => {
- function PageNo({post} : {post : any}) {
+ function PageNo({data} : {data : any}) {
 
     // const router = useRouter();
     // const pageNumber = router.query.pageNo;
     return (
         <>
-            {post.map( (curElem:any) => {
+            {data.map( (curElem:any) => {
             return(
                 <div className ="alert alert-primary" key={curElem.id}>
                     <h3>{curElem.name}</h3>
@@ -61,62 +32,99 @@ export default PageNo;
 
 
 
-
-
-
-
-
-
-
-
-
-            // <h1>Hello from { pageNumber } page</h1>
-
-
-
-
-
-
-
-
-
-
-
-// export default Post
-
-
-
-// export async function getStaticProps(context:any) {
+export async function getStaticProps(context:any) {
         
-//         const id = context.params.pageNo;
-//         const res = await fetch(`https://pro-component-express1o1.herokuapp.com/register/${id}`)
-//         const data = await res.json();
+        const id = context.params.pageNo;
+        const res = await fetch(`https://pro-component-express1o1.herokuapp.com/register/${id}`)
+        const data = await res.json();
             
-//         return {
-//             props: {
-//                 data,
-//             },
-//         }
+        return {
+            props: {
+                data,
+            },
+        }
+    }
+
+
+export const getStaticPaths = async () => {
+    const res = await fetch("https://pro-component-express1o1.herokuapp.com/register")
+    const data = await res.json();
+    
+    const paths = data.map( (curElem:any) =>  {
+        console.log(data);
+        if (curElem._id === undefined) {
+            return {
+                params: {
+                    pageno: curElem._id,
+                },
+            };        
+        }else {
+            return {
+                params: {
+                    pageno: curElem._id.toString(),
+                },
+            };        
+        }
+    })
+    
+    return {
+        props: {
+            paths,
+            fallback: false,
+        },
+  }    
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import type { NextPage } from 'next'
+
+
+// This function gets called at build time
+// export async function getStaticPaths() {
+//           // Call an external API endpoint to get posts
+//           const res = await fetch('https://pro-component-express1o1.herokuapp.com/register')
+//           const posts = await res.json()
+//           // Get the paths we want to pre-render based on posts
+//           console.log(posts);
+//           const paths = posts.map((post:any) => ({
+//             params: { id: posts._id.toString()},
+//           }))
+//           // We'll pre-render only these paths at build time.
+//           // { fallback: false } means other routes should 404.
+//         return { paths, fallback: false };
 //     }
 
-
-// export const getStaticPaths = async () => {
-//     const res = await fetch("https://pro-component-express1o1.herokuapp.com/register")
-//     const data = await res.json();
+// This also gets called at build time
+// export async function getStaticProps( params:any ) {
+//   // params contains the post `id`.
+//   // If the route is like /posts/1, then params.id is 1
+//       const res = await fetch(`https://.../posts/${params.id}`)
+//       const post = await res.json()
     
-//     const paths = data.map( (curElem:any) =>  {
-//         return{
-//             params: { 
-//                 pageNo : curElem._id.toString(),
-                
-//             },
-//         };
-//     })
-    
-//     return {
-//         props: {
-//             paths,
-//             fallback: false,
-//         },
-//   }    
-// };
+//       // Pass post data to the page via props
+//       return { props: { post } }
+// }
+            // <h1>Hello from { pageNumber } page</h1>
