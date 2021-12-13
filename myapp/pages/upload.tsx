1b4a24c6b5a0c63c4d4d12b560176717b64fe905
Harry1o1 <ajju40959@gmail.com>
2021-12-13 19:23:27 +0600

    Video111

--- myapp/pages/upload.tsx
+++ myapp/pages/upload.tsx
@@ -84,7 +84,7 @@ export default function Upload({data} : {data : any},{error}:{error : any}) {
                 .then((res) => {
                     console.log(res.data);
                     console.log(res);
-                    const data:any = res.json();
+                    const data:any = res.data;
                     if (res.status === 404) {
                         console.log('noooooo');
                     }
