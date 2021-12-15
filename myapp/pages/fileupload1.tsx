ccff823d4d9573c8d70d62b822e40342fb587981
Harry1o1 <ajju40959@gmail.com>
2021-12-15 09:35:40 +0600

    Video125

--- myapp/pages/fileupload1.tsx
+++ myapp/pages/fileupload1.tsx
@@ -3,17 +3,25 @@ let video = new FormData();
 
 
 
-function Fileupload1() {
+function Fileupload1({vufool} : {vufool : any}) {
     
     // let formData = new FormData();
     
     const handleVideo = (e:any) => {
         console.log(e.target.files[0]);
-        if (e.target && e.target.files[0]) {
-            video.append('file', e.target.files[0])
+        // if (e.target && e.target.files[0]) {
+        //     video.append('file', e.target.files[0])
             
+        // }
+        const vufool = e.target.files[0];
+        return {
+            props: {
+                vufool
+            }    
         }
     }
+        video.append('file', vufool)
+    
     const videUpload = () => {
         axios.post(
             'https://v2.cenvertapi.com/upload',
