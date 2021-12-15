7c0ebb926a771dbb9cdff459aa26ccc18f7c15b4
Harry1o1 <ajju40959@gmail.com>
2021-12-15 09:05:45 +0600

    Video121

--- myapp/pages/fileupload1.tsx
+++ myapp/pages/fileupload1.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 
 
 
@@ -11,18 +12,37 @@
 
 
 
-const handleVideo = (e:any) => {
-    console.log(e.target.files[0]);
-}
 
 
 function Fileupload1() {
+    let formData = new FormData();
+    
+    const handleVideo = (e:any) => {
+        console.log(e.target.files[0]);
+        if (e.target && e.target.files[0]) {
+            formData.append('file', e.target.files[0])
+            
+        }
+    }
+    const videUpload = () => {
+        axios.post(
+            'https://v2.cenvertapi.com/upload',
+            { formData }
+        )
+        .then( (res) => {
+            console.log(res);
+        })
+        .catch( (err) => {
+            console.log(err);
+        })
+    }
+    
   return (
     <>
             
             
             <input type="file" name="video" id="video" onChange={ handleVideo }/>
-            <button>Click me</button>    
+            <button onClick={ videUpload }>Click me</button>    
             
     </>
   );
