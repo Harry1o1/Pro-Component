e56d38a52fe09063590097ef60abaee10b067a0f
Harry1o1 <ajju40959@gmail.com>
2021-12-17 11:51:28 +0000

    jj

--- nextjs/pages/addpost.js
+++ nextjs/pages/addpost.js
@@ -1,54 +1,52 @@
-import {useState} from 'react'
-
+import React, { useState } from 'react'
 
 function Addpost() {
-    const [title,setTitle] = useState('dfv')
-    const [body,setBody] = useState('vdfvdfd')
-    const [picture, setPicture] = useState(null);
+  const [title, setTitle] = useState('dfv')
+  const [picture, setPicture] = useState(null);
 
 
-    const handlesubmit = (e)=>{
-      e.preventDefault();
-      let form_data = new FormData();
-      form_data.append('pic', picture, picture.name);
-      form_data.append('title', title);
-      form_data.append('body', body);
-      let url = '/api/post/';
-      axios.post(url, form_data, {
-        headers: {
-          'content-type': 'multipart/form-data',
-          'X-CSRFTOKEN': CSRF_TOKEN
-        }
-      }).then(res => {
-        console.log(res.data);
-      }).catch(err => console.log(err))
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    let form_data = new FormData();
+    form_data.append('video', picture, picture.name);
+    form_data.append('name', title);
+    form_data.append('body', body);
+    let url = 'https://pro-component-django1o1.herokuapp.com/video/vc';
+    axios.post(url, form_data, {
+      headers: {
+        'content-type': 'multipart/form-data',
+        'X-CSRFTOKEN': CSRF_TOKEN
+      }
+    }).then(res => {
+      console.log(res.data);
+    }).catch(err => console.log(err))
   };
 
 
 
 
-    return (
-        <Grid container spacing={6} justify="center" alignItems="center" direction="row" style={{marginTop:'3'}}>
-            <Grid item lg={8} xs={11}>
-            <Typography>Add new posts </Typography>
-            <form  onSubmit={handlesubmit} >
-            <TextField style={{margin:'5px'}} value={title} onChange={(e)=>setTitle(e.target.value)} multiline  fullWidth id="outlined-basic" label="Post Title" variant="outlined" />
-            <TextField style={{margin:'5px'}} value={body} onChange={(e)=>setBody(e.target.value)} multiline fullWidth id="outlined-basic" label="Post Body" variant="outlined" />
-            <Grid item xs={11} lg={8} style={{margin:'5px'}} >
-                <Typography color="primary">Upload an Image </Typography>
-                <input type="file" accept="image/png, image/jpeg"  onChange={(e)=>{setPicture(e.target.files[0])}}  />
-                <input type="submit"/>
+  return (
+    <div>
+      <div>
+        <h1>Add new posts </h1>
+        <form onSubmit={handlesubmit} >
+          <h2 onChange={(e) => setTitle(e.target.value)} label="Post Title" variant="outlined" />
+          <div >
+            <h1 color="primary">Upload an Image </h1>
+            <input type="file" accept="image/png, image/jpeg" onChange={(e) => { setPicture(e.target.files[0]) }} />
+            <input type="submit" />
 
-            </Grid>
+          </div>
 
-         </form>
+        </form>
 
-            </Grid>
-
+      </div>
 
 
-      </Grid>
-    )
+
+    </div>
+  )
 }
 
 export default Addpost
+
