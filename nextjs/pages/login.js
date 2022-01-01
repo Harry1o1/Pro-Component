add4673a3480fbf6802453599e42ad6b99b79093
Harry1o1 <ajju40959@gmail.com>
2022-01-01 09:28:37 +0600

    Still Refresh

--- nextjs/pages/login.js
+++ nextjs/pages/login.js
@@ -1,3 +1,4 @@
+import Navbar1 from "../components/Navbar/navbar1";
 
 
 
@@ -9,12 +10,52 @@
 function Login() {
   return (
     <>
-        <h1 className="text-center">Please log in</h1>
-        <form className="form-control">
-            <input className="form-control" type="text" name="username" id="username" value="" placeholder='Enter your Username'/>
-            <input className="form-control" type="text" name="password" id="password" value="" placeholder='Enter your password '/>
-            <input type="submit"/>
-        </form>
+        <header class="pro-header1-sm" id="pro-header1-sm">
+            
+            <Navbar1 />
+            
+            <div id="pro-header-maincontent">
+                <h1>Pro Header</h1>
+            </div>
+            
+        </header>
+        
+        <div id="hero">
+            <div id="pro-content-sm1" class="bg-dark">
+                Pro Content1
+            </div>
+            <div id="pro-content-sm2" class="bg-light">
+                Pro Content2
+            </div>
+            <div id="pro-content-sm3" class="bg-light">
+                Pro Content3
+            </div>
+            <div id="pro-content-sm4" class="bg-light">
+                Pro Content4
+            </div>
+            <div id="pro-content-sm5" class="bg-light">
+                Pro Content5
+            </div>
+            <div id="pro-content-sm6" class="bg-light">
+                Pro Content6
+            </div>
+            <div id="pro-content-sm7" class="bg-light">
+                Pro Content7
+            </div>
+            <div id="pro-content-sm8" class="bg-light">
+                Pro Content8
+            </div>
+            <div id="pro-content-sm9" class="bg-light">
+                Pro Content9
+            </div>
+            <div id="pro-content-sm10" class="bg-light">
+                Pro Content10
+            </div>
+        </div>
+        
+        <footer id="pro-footer1-sm">
+            
+        </footer>
     </>
   );
 }
--- nextjs/ux/globals.js
+++ nextjs/ux/globals.js
@@ -1,17 +1,41 @@
 const body = document.querySelector('body');
+const btn = document.querySelector('.pro_navbar_mb_btn');
+const small_ul = document.querySelector('.mb_list5');
+const navbar_sm = document.querySelector('.pro_navbar_main_container');
+const navbar_main_content = document.querySelector('.pro_navbar_mb_main-content');
+const sub = document.querySelector('.pro_navbar_sub-menu_mb');
+const page_menu = document.querySelector('.pro_navbar_page-menu_mb');
+
+btn.onclick = () => {
+        btn.classList.toggle('active');
+        navbar_sm.classList.toggle('active');
+};
 
 
 
-body.onscroll = () => {
-    console.log(`innerHeight is ${window.innerHeight}`);
-    console.log(`innerwight is ${window.innerWidth}`);
-    // console.log(`Sum is  ${window.innerHeight - window.innerWidth}`);
-    console.log(window.scrollY);
-}
 
+small_ul.onclick = () => {
+                const pp = document.querySelector('.pro_navbar_mb_main-content').childElementCount;
+                console.log(pp);
+                sub.classList.toggle('toggle');
+                page_menu.classList.toggle('toggle');
+                navbar_sm.classList.toggle('toggle');
+        }
+console.log('think hi one');
 body.onclick = () => {
     console.log('Onclick');
 }
+console.log(body.clientHeight);
+
+
+
+// body.onscroll = () => {
+//     console.log(`innerHeight is ${window.innerHeight}`);
+//     console.log(`innerwight is ${window.innerWidth}`);
+//     // console.log(`Sum is  ${window.innerHeight - window.innerWidth}`);
+//     console.log(window.scrollY);
+// }
+
 
 // body.onoffline = () => {
 //     console.log('Ofline');
@@ -24,4 +48,3 @@ body.onclick = () => {
 
 
 
-console.log(body.clientHeight);
