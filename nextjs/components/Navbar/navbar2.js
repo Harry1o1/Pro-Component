366367148fafc96c8f378486417adb85d36eb842
Harry1o1 <ajju40959@gmail.com>
2022-01-05 10:31:12 +0600

    Op

--- nextjs/components/Navbar/navbar2.js
+++ nextjs/components/Navbar/navbar2.js
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 // import Head from 'next/head'
-import styles from '../../styles/Navbar2.module.css'
 
 
 
@@ -13,7 +12,7 @@ function Navbar2() {
                     {/* Fixed navbar */}
             <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
               <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar2</a>
+                <a className="navbar-brand" href="#">Emon</a>
                 {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                           <span class="navbar-toggler-icon"></span>
                           <ion-icon id="navbar2-mb_close" name="close-outline"></ion-icon>
@@ -64,28 +63,27 @@ function Navbar2() {
               </div>
             </nav>
             <div id="wrapper">
-              <div className={styles.scrollmanu}>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    <a href className={styles.scrollmagic}> Page1 </a>
-                    
+              <div className="scrollmenu bg-light">
+                    <a href className="scrollmagic"> Page1 </a>
+                    <a href className="scrollmagic"> Page2 </a>
+                    <a href className="scrollmagic"> Page3 </a>
+                    <a href className="scrollmagic"> Page4 </a>
+                    <a href className="scrollmagic"> Page5 </a>
+                    <a href className="scrollmagic"> Page6 </a>
+                    <a href className="scrollmagic"> Page7 </a>
+                    <a href className="scrollmagic"> Page8 </a>
+                    <a href className="scrollmagic"> Page9 </a>
+                    <a href className="scrollmagic"> Page10 </a>
+                    <a href className="scrollmagic"> Page11 </a>
+                    <a href className="scrollmagic"> Page12 </a>
+                    <a href className="scrollmagic"> Page13 </a>
+                    <a href className="scrollmagic"> Page14 </a>
+                    <a href className="scrollmagic"> Page15 </a>
+                    <a href className="scrollmagic"> Page16 </a>
+                    <a href className="scrollmagic"> Page17 </a>
+                    <a href className="scrollmagic"> Page18 </a>
+                    <a href className="scrollmagic"> Page19 </a>
+                    <a href className="scrollmagic"> Page20 </a>
                   
                </div>
                <div id="marker" />
