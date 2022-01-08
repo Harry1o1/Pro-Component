c649eda675686ed783b074813866933e0a64df4e
Harry1o1 <ajju40959@gmail.com>
2022-01-08 16:31:07 +0600

    Responsive

--- nextjs/ux/globals.js
+++ nextjs/ux/globals.js
@@ -10,6 +10,32 @@ let menu = document.querySelector('span');
 let navbar2 = document.querySelector('body');
 let marker = document.querySelector('#marker');
 let items = document.querySelectorAll('.op');
+let navbar = document.querySelector('.scroll');
+
+
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+// Get the offset position of the navbar
+let sticky = navbar.offsetTop;
+// let sticky = navbar.getBoundingClientRect();
+
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+
+
+
+
 
 // Scroll💯
     function indicator(e) {
