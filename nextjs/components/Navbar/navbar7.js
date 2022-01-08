1a15927d7d66abb96fdc3dd236b271b8595dcf5c
Harry1o1 <ajju40959@gmail.com>
2022-01-08 23:54:37 +0600

    Major changes in ui you must check previuos commit for old ui and still  developing

--- nextjs/components/Navbar/navbar7.js
+++ nextjs/components/Navbar/navbar7.js
@@ -215,16 +215,3 @@ export default Navbar7;
 
 
 
-
-
-<!-- Block level -->
-<div class="row">
-  <div class="col-2 text-truncate">
-    Praeterea iter est quasdam res quas ex communi.
-  </div>
-</div>
-
-<!-- Inline level -->
-<span class="d-inline-block text-truncate" style="max-width: 150px;">
-  Praeterea iter est quasdam res quas ex communi.
-</span>
