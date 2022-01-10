058481c134846166dd7ced8bc9517339dca39c37
Harry1o1 <ajju40959@gmail.com>
2022-01-11 01:23:11 +0600

    nextjs

--- nextjs/pages/goals.js
+++ nextjs/pages/goals.js
@@ -7,18 +7,6 @@ function Goals() {
             <h1>Welcome to Todo</h1>
             <p>Time wait for no one</p>
          </div>
-                    <span id="op1">
-                <ion-fab-button>
-                  <ion-icon name="arrow-up-circle"></ion-icon>
-                </ion-fab-button>
-            </span>  
-            
-            
-            <span id="op2">
-                    <ion-fab-button>
-                      <ion-icon name="add"></ion-icon>
-                    </ion-fab-button>
-            </span>  
             
             
           
