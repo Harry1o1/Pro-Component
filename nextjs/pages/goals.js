401312fe7582f79f621ba0a60d10eddabc27c16e
Harry1o1 <ajju40959@gmail.com>
2022-01-11 01:17:03 +0600

    nextjs

--- nextjs/pages/goals.js
+++ nextjs/pages/goals.js
@@ -3,12 +3,32 @@ function Goals() {
   return (
     <>
         {/* Begin page content */}
-        <main id="hero" className="bg-light main">
           <div className="bg-white" id="container">
             <h1>Welcome to Todo</h1>
             <p>Time wait for no one</p>
-          </div>
-        </main>
+         </div>
+                    <span id="op1">
+                <ion-fab-button>
+                  <ion-icon name="arrow-up-circle"></ion-icon>
+                </ion-fab-button>
+            </span>  
+            
+            
+            <span id="op2">
+                    <ion-fab-button>
+                      <ion-icon name="add"></ion-icon>
+                    </ion-fab-button>
+            </span>  
+            
+            
+          
+          
+          
+          
+          
+          
+          
+          
     </>
   );
 }
