b2b957117108c6ee97caf76ba20a0be1127d3943
Harry1o1 <ajju40959@gmail.com>
2022-01-23 20:55:49 +0600

    Happy 500 commit

--- nextjs/ux/globals.js
+++ nextjs/ux/globals.js
@@ -11,11 +11,21 @@ let navbar2 = document.querySelector('body');
 let marker = document.querySelector('#marker');
 let items = document.querySelectorAll('.scroll_li_a');
 let navbar = document.querySelector('.sidebar-sm');
-let segment_items = document.querySelectorAll('.segment-item');
-let segment_indicator = document.querySelector('#segment-indicator');
+let segment_items2 = document.querySelectorAll('.segment-item2');
+let segment_indicator2 = document.querySelector('#segment-indicator2');
+let segment_items3 = document.querySelectorAll('.segment-item3');
+let segment_indicator3 = document.querySelector('#segment-indicator3');
 
 
 
+
+
+
+
+
+
+
+// Sticky💎💎💎
 if (navbar !== null) {
     let sticky = navbar.offsetTop;
     function myFunction() {
@@ -42,12 +52,6 @@ if (btn !== null) {
 
 
 
-if (navbar === null) {
-    console.log('null');
-} else {
-    console.log('op');
-    
-}
 
 
 
@@ -56,16 +60,11 @@ if (navbar === null) {
 
 
 
-// Scroll💯
+// Horizental Scrolling💯
 if (marker !== 'undefined' && items !== 'undefined') {
     function indicator(e) {
-
-        
-
         let rule1 = 11;
-
         let rule2 = 9;
-        
         let ifcon = 100;
         
         marker.style.left = e.offsetLeft - rule1 + 'px';
@@ -92,53 +91,75 @@ if (marker !== 'undefined' && items !== 'undefined') {
         })
         
     });
+}
+
+
+
+
+
+
+// Segment2🥳🥳🥳
+if (segment_items2 !== 'undefined' && segment_indicator2 !== 'undefined') {
+    function indicator(e) {
+        let rule = 45;
+        let width_rule = 0;
+        
+        segment_indicator2.style.left = e.offsetLeft - rule + 'px';
+        
+        
+        segment_indicator2.style.width = e.offsetWidth + width_rule + 'px';
+        console.log(segment_indicator2.style.left);
+        console.log(segment_indicator2.style.width);
+    };
+    segment_items2.forEach(link => {
+        link.addEventListener('click', (e) => {
+            indicator(e.target);
+            
+        })
+        link.addEventListener('dbclick', (e) => {
+            indicator(e.target);
+        })
+        
+    });
     
 }
-if (segment_items !== 'undefined' && segment_indicator !== 'undefined') {
+
+
+// Segment3🥳🥳🥳
+if (segment_items3 !== 'undefined' && segment_indicator3 !== 'undefined') {
     function indicator(e) {
-        let ifcon1 = 50;
+        let ifcon1 = 52;
         let ifcon2 = 140;
         let ifcon3 = 160;
-        let first = 1.3;
-        let second = 8.9;
-        let third = 15.6;
         
+        let first = 1.5;
+        let second = 8.3;
+        let third = 15.2;
         
         let width_rule = 20;
         
-        // segment_indicator.style.left = e.offsetLeft - rule1 + 'px';
+        // segment_indicator3.style.left = e.offsetLeft + 'px';
         
         if (e.offsetLeft > ifcon1 ) {
             if (e.offsetLeft > ifcon2 ) {
                 console.log('80');
-                segment_indicator.style.left = second + 'rem';
-            
+                segment_indicator3.style.left = second + 'rem';
             } 
             if (e.offsetLeft > ifcon3) {
                 console.log('80+');
-                segment_indicator.style.left = third + 'rem';
-            
+                segment_indicator3.style.left = third + 'rem';
             }
-            
         }else{
             console.log('1st');
-            // segment_indicator.style.right = e.offsetLeft - rule1 + 'px';
-            segment_indicator.style.left = first + 'rem';
-            
+            segment_indicator3.style.left = first + 'rem';
             
         }
         
-        
-        
-        segment_indicator.style.width = e.offsetWidth + width_rule + 'px';
-        
-        
-        console.log(segment_indicator.style.left);
-        console.log(segment_indicator.style.width);
-        
-        
+        segment_indicator3.style.width = e.offsetWidth + width_rule + 'px';
+        console.log(segment_indicator3.style.left);
+        console.log(segment_indicator3.style.width);
     };
-    segment_items.forEach(link => {
+    segment_items3.forEach(link => {
         link.addEventListener('click', (e) => {
             indicator(e.target);
             
@@ -157,6 +178,12 @@ if (segment_items !== 'undefined' && segment_indicator !== 'undefined') {
 
 
 
-
+// Others😅😅😅
 console.log(body.clientHeight);
 Robin('Op Argument');
+if (navbar === null) {
+    console.log('null');
+}else {
+    console.log('op');
+    
+}
